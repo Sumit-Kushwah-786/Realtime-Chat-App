e3e93288dcc08f5ca613a03680309830c33dde1e
Sumit-Kushwah-786 <sumitkushwah33221@gmail.com>
2025-05-07 19:05:01 +0530

    new change

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../Backend/Frontend/dist", // Ensuring correct folder structure for production
-  },
 });
